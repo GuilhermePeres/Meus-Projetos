2e8cc81015d856db584f012adb865875a61a63bc
GuilhermePeres <guilherme.peresmn@gmail.com>
2021-09-13 15:07:45 -0300

    Initial commit

---
+++ Portfólio Parallax/main.js
@@ -0,0 +1,6 @@
+function hover() {
+    document.getElementById('text-about-me').style.visibility = 'visible';
+    document.getElementById('animation1').className = 'animation-paragraph1';
+    document.getElementById('animation2').className = 'animation-paragraph2';
+    document.getElementById('animation3').className = 'animation-paragraph3';
+}
